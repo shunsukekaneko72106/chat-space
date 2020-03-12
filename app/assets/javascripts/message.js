f6f5ce335806783ec6f1f8fe6c7ede96a2539c00
shunsukekaneko72106 <61620829+shunsukekaneko72106@users.noreply.github.com>
2020-03-12 14:19:29 +0900

    変更しました

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,68 @@
+
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+      `<div class="message">
+         <div class="message-info">
+           <div class="upper-message__chat-name">
+             ${message.user_name}
+           </div>
+           <div class="upper-message__chat-date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+         <img src=${message.image} >
+       </div>`
+     return html;
+   } else {
+     var html =
+      `<div class="message">
+         <div class="message-info">
+           <div class="upper-message__chat-name">
+             ${message.user_name}
+           </div>
+           <div class="upper-message__chat-date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+       </div>`
+     return html;
+   };
+ }
+  $('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html); 
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight }, "first");
+      $('.submit-btn').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+});
+
+  
+
